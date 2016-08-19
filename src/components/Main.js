d294be9bff9b7094bfe95ecf53a909288e5e6fec
Marian Gaebler <marian.gaebler@gmail.com>
2016-08-19 21:42:58 +0200

    more scaffolding

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,15 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.sass');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+
+        <button className='controll true' />
+        <button className='controll false' />
       </div>
     );
   }
